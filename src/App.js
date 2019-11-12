4a139fa57fae5346a3d9c81f22510ffb40141233
Your Name <you@example.com>
2019-11-12 20:54:20 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      topText: "",
+      bottomText: "",
+	  file: null
+    };
+  }
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+  handleClick = event => {
+    this.setState({ file: URL.createObjectURL(event.target.files[0])});
+  };
+  
+  render() {
+    return (
+      <div>
+		<div className="upload">
+		<input
+			type="file"
+			onChange={this.handleClick}
+		 />
+		 </div>
+		 <div className="meme-form">
+          <input
+            type="text"
+            name="topText"
+            placeholder="top text"
+            onChange={this.handleChange}
+            value={this.state.topText}
+          />
+          <br />
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="bottom text"
+            onChange={this.handleChange}
+            value={this.state.bottomText}
+          />
+          <br />
+        </div>
+        <div className="meme">
+          <h2
+            className="top"
+          >
+            {this.state.topText}
+          </h2>
+          <img src={this.state.file} alt="" />
+          <h2
+            className="bottom"
+          >
+            {this.state.bottomText}
+          </h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
